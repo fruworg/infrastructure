f8b73add48c41df3aab7d6fae7d3790cbdb589c3
Руслан <im@fruw.org>
2023-09-24 18:56:51 +0600

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,21 +4,21 @@ source /root/.bashrc
 
 # Iterate through all folders in /opt
 for FOLDER in /opt/*; do
-    if [ -d "$FOLDER" ]; then
-        echo "Processing folder: $FOLDER"
+  if [ -d "$FOLDER" ]; then
+    echo "Processing folder: $FOLDER"
 
-        # Navigate into the folder
-        cd "$FOLDER" || continue
+    # Navigate into the folder
+    cd "$FOLDER" || continue
 
-        # Pull Docker Compose images
-        docker compose pull
+    # Pull Docker Compose images
+    docker compose pull
 
-        # Bring up Docker Compose services in detached mode
-        docker compose up -d
+    # Bring up Docker Compose services in detached mode
+    docker compose up -d
 
-        # Return to the previous directory
-        cd -
-    fi
+    # Return to the previous directory
+    cd -
+  fi
 done
 
 docker system prune -a -f
